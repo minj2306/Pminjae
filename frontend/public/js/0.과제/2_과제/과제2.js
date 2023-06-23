cef38a8d7fdca4e8617195b22d0f27d45c521cbe
504 <504@DESKTOP-8HL92AU>
2023-06-23 11:55:31 +0900

    과제즁

--- frontend/public/js/0.과제/2_과제/과제2.js
+++ frontend/public/js/0.과제/2_과제/과제2.js
@@ -1,3 +1,32 @@
 /**
  * 
  */
+
+let joinid =[]
+let joinpassword = []
+
+function join( ){
+	 
+	
+	console.log('회원가입')
+	//input 태그 호출
+	let idInput = document.querySelector('.id')
+	let passwordInput = document.querySelector('.password')
+	console.log(idInput)
+	console.log(passwordInput)
+	// 입력 받은 값 호출
+	let comid = idInput.value;
+	let compw = passwordInput.value;
+	console.log(comid);
+	console.log(compw);
+	//배열등록
+	 
+	
+	//입력값 초기화
+	idInput.value=''
+	passwordInput.value=''
+	
+	
+	
+	
+}
