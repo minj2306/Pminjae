0ea262a03d6da49b55c29eb2b3b74302907da0d2
minjaepark <minjaepark@121.139.152.4>
2023-06-24 14:57:09 +0900

    과제2 하는중

--- frontend/public/js/0.과제/2_과제/과제2.js
+++ frontend/public/js/0.과제/2_과제/과제2.js
@@ -20,14 +20,26 @@ function join( ){
 	console.log(comid);
 	console.log(compw);
 	//배열등록및 유효성 검사
-	if(comid.length < 8 || compw.length < 8){
-			alert('아이디 비밀번호는 8글자 이상이어야 합니다.'); }
-	else{joinid.push(idInput);
-		joinpassword.push(passwordInput); 
-		alert('회원가입 성공!')}
+	if(comid==` `|| compw==``){
+		alert('학번을 입력해주세요 ');
+	}
+	else if (comid.length < 8 || compw.length<8){
+		alert('아이디 비밀번호는 8글자 이상이어야 합니다. ');
+	}
+	else{joinid.push(idInput)
+		joinpassword.push(passwordInput)
+		alert('회원가입 성공 ')}
 	 console.log(joinid)
 	console.log(joinpassword )
 	//입력값 초기화
 	idInput.value=''
 	passwordInput.value=''
+}
+
+function log_in(){
+	 let login_idInput = document.querySelector('login_id')
+	 let login_pwInput = document.querySelector('login_pw')
+	 
+	 
+	 
 }
