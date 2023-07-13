cb5f89f6e19dc77b48bce9b0eec93d53422beb0a
504 <504@DESKTOP-8HL92AU>
2023-07-13 18:06:55 +0900

    자바 과제 1

---
+++ beckend/src/과제/과제1/과제1.java
@@ -0,0 +1,54 @@
+package 과제.과제1;
+
+public class 과제1 {
+	
+	public static void main(String[] args) {
+		/*
+		 문제 1 
+		 	풀이 : 초기값이 저장 안되면 에러남
+		 	답 	: 4번
+		 문제 2
+		  	풀이 : 변수 이름은 숫자로 시작 x 문자로 시작해야함
+		  	답	: 3 , 4 , 5 , 6
+		 문제 3
+		 	풀이 : 
+		 	답	: 1byte		2byte	4byte	8byte
+		 	
+		 	정수	  byte		short	int		long
+		 					char
+		 	실수	  					float	double
+		 	
+		 	논리	  boolean
+		 	
+		 문제 4
+		 	풀이	: 
+		 	답	: 타입 		int , double
+		 		  변수이름 	age , price
+		 		  리터럴		10	, 3.14
+		 문제 5
+		 	풀이 : 자동 타입 변환은 작은것을 큰것에 변환 할떄 자동으로 가능
+		 			byte<short , char < int < long < float < double
+		 			short가 char 보다 작음
+		 	답	: 3번
+		 문제 6
+		 	풀이 : 강제 타입 변환은 큰것을 작은것에 변환 double<int
+		 	답	: 3번
+		 문제 7
+		 	풀이 : 공백을 표현할땐 "" -> 큰따옴표 3두개
+		 	답	: 3번
+		 문제 8
+		 	풀이 : Scanner.nextLine()은 입력값을 받는다
+		 	답	: 4번
+		 문제 9
+		 	풀이 : 실수 타입에 정수+실수는 둘다 실수로 변환 (double)
+		 	답	: 4번
+		 문제 10
+		 	풀이 : int는 Integer
+		 	답	: 2번
+		 문제 11
+		 	풀이 :{ }안에서 선언된 변수는 해당 { } 안에서만 사용 가능
+		 	답	: 13줄 16줄
+		 * */
+	}
+	
+}
