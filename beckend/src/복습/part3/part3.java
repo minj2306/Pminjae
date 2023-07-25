1107221ed6b8b68bce2e86bf283b56f9d5261428
504 <504@DESKTOP-8HL92AU>
2023-07-25 21:55:51 +0900

    ㅂㅂ

--- beckend/src/복습/part3/part3.java
+++ beckend/src/복습/part3/part3.java
@@ -11,12 +11,11 @@ public class part3 {
 		String[] 재고관리 = {"10,0,300,콜라","10,0,200,환타","10,0,100,사이다"};
 		
 		while (true) {
-			
-			System.out.println("--------매뉴---------");
+			System.out.println("--------------메뉴-------------");
 			System.out.println("1.콜라[300] 2.환타[200] 3.사이다[100] 4.결제");
-			System.out.println(">>>>>>선택"); int ch = scanner.nextInt();
+			System.out.println(">>>>>>선택");int ch = scanner.nextInt();
 			
-			if( ch==1 || ch==2 || ch==3) {
+			if(ch==1||ch==2||ch==3) {
 				
 				String 제품정보 = 재고관리[ch-1];
 				int 재고 = Integer.parseInt(제품정보.split(",")[0]);
@@ -26,31 +25,31 @@ public class part3 {
 				
 				if(재고>0) {
 					재고--; 바구니++;
-					System.out.println(ch+" 번 제품 바구니에 담았습니다.");
+					System.out.println(name+ " 제품 담았습니다.");
 				}
-				else {System.out.println(ch+"번 제품 재고부족");}
-				
-				재고관리[ch-1]=재고+","+바구니+","+가격+","+name;
+				else {
+					System.out.println(name+" 제고 부족합니다.");
+				}
+				재고관리[ch-1] = 재고+","+바구니+","+가격+","+name;
 			}
 			else if(ch==4) {
-				System.out.println("---------장바구니 현황--------");
-				System.out.printf("%10s %10s %10s\n","제품명","수량","가격");
+				System.out.println("---------장바구니 현황----------");
+				System.out.printf("%10s %10s %10s \n","제품명","수량","가격");
 				
-				for(int i = 0 ; i<재고관리.length; i++) {
+				for( int i = 0; i<재고관리.length; i++) {
+					
 					int 바구니 = Integer.parseInt(재고관리[i].split(",")[1]);
 					int 가격 = Integer.parseInt(재고관리[i].split(",")[2]);
 					String name = 재고관리[i].split(",")[3];
-					
 					if(바구니>0) {
-						System.out.printf("%10s %10d %10d\n", name,바구니,바구니*가격);
+						System.out.printf("%10s %10s %10s \n",name,바구니,바구니*가격);
+
 					}
-					
 				}
-				
+			
 			}
-			
-			
-		}//wh e
+		}	
+		
 		
 	}//main e
 		
