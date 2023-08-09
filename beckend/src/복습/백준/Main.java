03d8f6227abbffcc37802751a320ef50ba3b489d
504 <504@DESKTOP-SI28KUP>
2023-08-09 17:59:11 +0900

    8/9

---
+++ beckend/src/복습/백준/Main.java
@@ -0,0 +1,16 @@
+package 복습.백준;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println( x*y );
+		
+	}
+}
