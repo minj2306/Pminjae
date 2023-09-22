0c5bfb7d6497b1d684c00fd2ca5e163c1aeeddc4
Pminjae <alswo2035@naver.com>
2023-09-22 14:38:39 +0900

    9/22

--- jspweb/src/main/webapp/js/payment.js
+++ jspweb/src/main/webapp/js/payment.js
@@ -28,18 +28,23 @@ function getPoint(){
 	
 	if( loginState == false ){ return; }
 
+	let returnData = false;
+	
 	$.ajax({
 	url : "/jspweb/PointController",
 	method : "get",
+	async :false , // ajax 가 응답이 올때까지 대기상태 = 동기식
 	data : { type : "getPoint" } ,
 	success : r => {
 		console.log(r);
+		returnData = r;
 	} ,
 	error : e =>{
 		console.log(e);
 	}
 	})
 	
+	return returnData; // ajax 결과를 리턴하는 방식
 }
 
 function getPointList(){
@@ -63,15 +68,68 @@ function getPointList(){
 }
 
 
+//-----------------------------결제연동---------------------
 
+// 1. 
+IMP.init('imp42611532') // 아임포트 콘솔에서 확인
 
+ function requestPay() {
+	 
+	 // 2-1 : 만약에 결제방식을 선택 안했으면
+	 if( pay_method == ''){
+		 alert('결제 방식을 선택해주세요'); return;
+	 }
+	 if( amount == 0 ){
+		 alert('결제 금액을 선택해주세요'); return;
+	 }
+	 
+	 // 2-2 : 주문번호 만들기
+	 	// * 고유성/중복X/PK/식별키
+	 	// 1. 날짜(밀리초)+상품코드+회원번호 조합해서 설계
+		// 2. 회사전화번호+사업자번호 조합해서 설계
+			// ---- 등등
+		let now = new Date(); console.log(now); // 현재날짜
+		let time = now.getTime();	console.log(time); // 현재시간[밀리초]
+		let merchant_uid = "p"+time+"-"+loginMid; console.log( merchant_uid )
+	 	
+	 	
+    IMP.request_pay({
+      pg: "html5_inicis.INIBillTst", // 아임포트 콘솔에서 확인
+      pay_method: pay_method,
+      merchant_uid: "ORD20180131-0000011",   // 주문번호
+      name: "포인트결제",
+      amount: amount ,                         // 숫자 타입
+      buyer_email: "gildong@gmail.com",
+      buyer_name: "박민재",
+      buyer_tel: "010-4242-4242",
+      buyer_addr: "경기도 안산시 상록구",
+      buyer_postcode: "12345"
+    }, function (rsp) { // callback
+      //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+      console.log(rsp);
+      if(rsp.success){ // 결제성공
+	  }else{ // 결제 실패 [ 테스트 : 실패가 성공이라는 가정]
+		  alert('결제성공')
+		  
+		  // 1. 포인트 적립 기능ㅊ처리
+		  let result = setPoint( amount , '포인트결제');
+		  
+		  // 2. 결제내역 테이블 기능처리 [구현x] 
+	  }
+      
+    });
+  }
 
+let pay_method = ''; // 결제방식
+function onPayMethod( method ){
+	pay_method = method;
+	
+}
 
-
-
-
-
-
+let amount = 0; // 결제금액
+function onAmount(value){
+	amount = value;
+}
 
 
 
--- jspweb/src/main/webapp/member/payment.jsp
+++ jspweb/src/main/webapp/member/payment.jsp
@@ -5,14 +5,39 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link href ="../css/payment.css" rel="stylesheet" />
+
 </head>
 <body>
 <%@include file="../header.jsp" %>
 
 	<div class="webcontainer">
-		<button type="button">결제하기</button>
+		<div class="wrap">
+			<div class="paymenthaeder">
+			결제 페이지
+			</div>
+			<div class="patmentspc">
+				<div class="amountselect">
+					<button onclick="onAmount(1000)" type="button">1000포인트</button>
+					<button onclick="onAmount(5000)" type="button">5000포인트</button>
+					<button onclick="onAmount(10000)" type="button">10000포인트</button>
+					<button onclick="onAmount(50000)" type="button">50000포인트</button>
+					<button onclick="onAmount(100000)" type="button">100000포인트</button>
+				</div>
+				<div class="methodselect">
+					<button onclick="onPayMethod('card')" type="button">신용카드</button>
+					<button onclick="onPayMethod('trans')" type="button">실시간 계좌이체</button>
+					<button onclick="onPayMethod('vbank')" type="button">가상계좌</button>
+					<button onclick="onPayMethod('phone')" type="button">휴대폰 소액결제</button>
+				</div>
+				<div class="finalbtn">
+					<button class="paybtn" onclick="requestPay()" type="button">결제하기</button>
+				</div>
+			</div>
+		</div>
 	</div>
-
+	
+<script src="https://cdn.iamport.kr/v1/iamport.js"></script>
 <script src="../js/payment.js" type="text/javascript"></script>
 </body>
 </html>
